899376a72c9259a29907d85bd1673965102299f8
bechir <bechirhadiden8@gmail.com>
2024-12-21 14:14:15 +0100

    commi securite3

--- src/main/java/com/bechir/sante/security/JWTAuthorizationFilter.java
+++ src/main/java/com/bechir/sante/security/JWTAuthorizationFilter.java
@@ -1,16 +1,14 @@
 package com.bechir.sante.security;
 
 import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.auth0.jwt.JWT;
